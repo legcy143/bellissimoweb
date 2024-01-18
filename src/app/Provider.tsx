d32661474b0224f64cf12e8c9673ab25ba373314
legcy <legcy143@gmail.com>
2024-01-18 22:06:59 +0530

    fixing

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,12 +1,10 @@
 "use client"
 
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
-import { useRouter } from "next/navigation"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import axios from "axios"
 
 export function Provider({ children, ...props }: ThemeProviderProps) {
     
